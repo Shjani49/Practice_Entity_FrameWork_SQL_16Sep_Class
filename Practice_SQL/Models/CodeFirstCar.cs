aad7abd15d19052d1a9e557f21a16d05147bb4ea
Shjani49 <shjani49@gmail.com>
2020-09-17 10:00:35 -0600

    Up to 4c (model created);

--- Practice_SQL/Models/CodeFirstCar.cs
+++ Practice_SQL/Models/CodeFirstCar.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Practice_SQL.Models
 {
+    // Declares the name of the database table.
+    [Table("code_first_car")]
     public partial class CodeFirstCar
     {
-        
+        // Declare a primary key.
+        [Key]
+        // AUTO_INCREMENT (according to Google, we'll find out).
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        // Declare the column name and the data type.
+        // MySQL data type, NOT C# data type.
+        [Column("id", TypeName = "int(10)")]
+        // Declare the C# property that will map onto that database column.
+        public int ID { get; set; }
+
+        [Column("manufacturer", TypeName = "varchar(30)")]
+        // Specifies NOT NULL on nullable types.
+        // Ints do not require this to be NOT NULL as they are not nullable.
+        // You must make a nullable int (int?) in order to specify NULL.
+        [Required]
+        public string Manufacturer { get; set; }
+
+        [Column("model", TypeName = "varchar(30)")]
+        [Required]
+        public string Model { get; set; }
+
+        [Column("trim_level", TypeName = "varchar(30)")]
+        [Required]
+        public string TrimLevel { get; set; }
+
+        [Column("colour", TypeName = "varchar(10)")]
+        [Required]
+        public string Colour { get; set; }
+
+        [Column("odometer", TypeName = "int(10)")]
+        // To make a NULL int, we can specify the data type as "int?".
+        public int Odometer { get; set; }
     }
 }
--- Practice_SQL/Program.cs
+++ Practice_SQL/Program.cs
@@ -16,7 +16,7 @@ namespace Practice_SQL
             1) Add new "Models" folder.
             2) Add classes to "Models".
                 2a) Add a public partial "ThingContext.cs" class inheriting from DbContext.
-                2b) Add column classes.
+                2b) Add public partial column classes.
             3) Set up skeleton context class.
                 3a) Create constructors.
                     3a1) Create a empty default constructor.
@@ -25,6 +25,11 @@ namespace Practice_SQL
                     3b1) Add an OnConfiguring() override that accepts a DbContextOptionsBuilder, if that option builder's "IsConfigured" property is false, declare the connection string for the database.
                     3b2) Add an OnModelCreating override that accepts a ModelBuilder, call OnModelCreatingPartial() at the end.
                     3b3) Add a partial OnModelCreatingPartial override call at the end with no body.
+            4) Set up skeleton column classes.
+                4a) Declare table name.
+                4b) Declare columns (names and types).
+                4c) Declare foreign keys (if applicable).
+
             */
 
         }
