b8cc0c4cc53c455de47c7b7f405316d301d872f1
Shjani49 <shjani49@gmail.com>
2020-09-17 09:48:44 -0600

    Up to 3b3 (skeleton context);

--- Practice_SQL/Models/CarsCodeFirstContext.cs
+++ Practice_SQL/Models/CarsCodeFirstContext.cs
@@ -10,6 +10,33 @@ namespace Practice_SQL.Models
         public CarsCodeFirstContext()
         {
         }
+        public CarsCodeFirstContext(DbContextOptions<CarsCodeFirstContext>options) : base(options)
+        {
+
+        }
+        // Called when we're configuring a database connection.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // If we aren't already configured.
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Initialize the connection to a MySQL server.
+                optionsBuilder.UseMySql("CONNECTION_STRING_HERE", x => x.ServerVersion("DB_SERVER_VERSION_HERE"));
+            }
+        }
+
+        // Called when we're doing database migrations, etc.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // We will add model-related stuff here in the future.
+
+
+            // Call the partial method in case we add some stuff to another file later.
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        // Not technically needed, but the scaffolding generates it for later use, so we can add it if we want for future-proofing.
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
       
 }
--- Practice_SQL/Program.cs
+++ Practice_SQL/Program.cs
@@ -10,14 +10,21 @@ namespace Practice_SQL
         {
             // Using "Code First" techniques allow us to make changes and even create entire databases without writing a single line of SQL.
             /*
+            Ensure you have the Microsoft Design and Pomelo MySql packages installed as per the notes on the master branch.
+            Throughout this, if you encounter red lines in any file, use Ctrl+. to add the proposed "using" statements.
+               
             1) Add new "Models" folder.
             2) Add classes to "Models".
-                2a) Add "ThingContext.cs" class inheriting from DbContext.
+                2a) Add a public partial "ThingContext.cs" class inheriting from DbContext.
                 2b) Add column classes.
             3) Set up skeleton context class.
                 3a) Create constructors.
                     3a1) Create a empty default constructor.
                     3a2) Create a constructor that acceps a DbContextOptions<ThisContext> options, and calls the base constructor.
+                3b) Create configuration methods.
+                    3b1) Add an OnConfiguring() override that accepts a DbContextOptionsBuilder, if that option builder's "IsConfigured" property is false, declare the connection string for the database.
+                    3b2) Add an OnModelCreating override that accepts a ModelBuilder, call OnModelCreatingPartial() at the end.
+                    3b3) Add a partial OnModelCreatingPartial override call at the end with no body.
             */
 
         }
